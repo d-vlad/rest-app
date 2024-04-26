359abe385e5dc964b60bd56a4d5ed098fc8e5000
Dulgheru Vladut Florin <dulgheruvladut@gmail.com>
2024-04-26 14:01:06 +0300

    first commit with the app + swagger

---
+++ src/main/java/org/rest/app/configuration/AppConfiguration.java
@@ -0,0 +1,10 @@
+package org.rest.app.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.rest")
+public class AppConfiguration {
+
+}
